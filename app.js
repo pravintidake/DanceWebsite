af7ca1ee54492847a018e771a4784c24a4e7283d
Pravin Tidake <ashirvadtidake@gmail.com>
2021-10-13 00:47:13 +0530

    new update for .env and database string

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 //Connect to the database
 main().catch(err => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/ADADaceWebsite');
+  await mongoose.connect(process.env.ConnectionStr);
 }
 
 //Define New Mongoose Schema
