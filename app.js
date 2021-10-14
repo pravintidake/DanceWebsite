b2f1d0aa6bcfe6449d72cefa924c38a253fd528b
Pravin Tidake <ashirvadtidake@gmail.com>
2021-10-14 10:42:11 +0530

    ui fix and contact page fixed

--- app.js
+++ app.js
@@ -15,10 +15,22 @@ async function main() {
 
 //Define New Mongoose Schema
 const contactSchema = new mongoose.Schema({
-    name: String,
-    phone: String,
-    email: String,
-    address: String,
+    name:{
+        type : String,
+        required : true
+    },
+    phone:{
+        type : String,
+        required : true
+    },
+    email:{
+        type : String,
+        required : true
+    },
+    address:{
+        type : String,
+        required : true
+    },
     description: String
   });
 
@@ -47,10 +59,34 @@ app.get('/contact', (req, res)=>{
 app.post('/contact', (req, res)=>{
     var myData = new Contact(req.body);
     myData.save().then(() => {
-        res.status(200).send("Your Data Has Been Saved Successfully...");
+        res.status(200).send(`<h1>Thank You Contact Us... We will work on your request.</h1>
+        <a href="/" style="border-radius: 10px;
+        padding: 3px 35px;
+        margin: 5px auto;
+        margin-bottom: 20px;
+        cursor: pointer;
+        color: white;
+        background-color: #3d0407;
+        font-family: 'Teko', sans-serif;
+        font-size: 1rem;
+        border: 2px solid #3d0407;
+        text-decoration: none;">Go Back</a>`);
         console.log("Data saved successfully...");
     }).catch(() => {
-        res.status(400).send("Ooppsss...Your Data not saved to the database...");
+        // res.status(400).send("Ooppsss...Your Data not saved to the database...");
+        res.status(400).send(`<h1>Ooppsss...Sorry! Your Request Not Been Registered. Please Try Again Later.</h1>
+        <a href="/" style="border-radius: 10px;
+        padding: 3px 35px;
+        margin: 5px auto;
+        margin-bottom: 20px;
+        cursor: pointer;
+        color: white;
+        background-color: #3d0407;
+        font-family: 'Teko', sans-serif;
+        font-size: 1rem;
+        border: 2px solid #3d0407;
+        text-decoration: none;">Go Back</a>`);
+        console.log("Data not saved...");
     })
     //res.status(200).render('contact.pug');
 })
